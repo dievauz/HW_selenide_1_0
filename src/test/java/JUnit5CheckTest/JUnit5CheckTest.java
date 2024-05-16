d42a65684d5c4bd1dc1868512e7d20a7ed776294
dievauz <167197411+dievauz@users.noreply.github.com>
2024-05-16 23:08:52 +0300

    initial commit

---
+++ src/test/java/JUnit5CheckTest/JUnit5CheckTest.java
@@ -0,0 +1,42 @@
+package JUnit5CheckTest;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class JUnit5CheckTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void JUnit5Check() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("softassertions");
+        $("[data-filterable-for = wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+
+        $("[data-filterable-for = wiki-pages-filter]").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"
+        ));
+    }
+}
